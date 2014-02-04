6d8a0be7067075a41a1333c34baf33d2e71079c7
ravenjohn <rjlagrimas08@gmail.com>
2014-02-04 23:33:00 +0800

    cylinder : Fixed lighting

--- cylinder-5/script.js
+++ cylinder-5/script.js
@@ -1,5 +1,4 @@
 ;(function (root) {
-    'use strict';
     var main = function () {
             var gl,
                 temp = 0,
@@ -84,31 +83,40 @@
             gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(Array.apply(0, Array(vertices.length / 3)).map(function () {return temp+=1;})), gl.STATIC_DRAW);
             gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-            mat4.rotateX(modelMatrix = mat4.create(), modelMatrix, glMatrix.toRadian(angle));
-            mat4.rotateY(modelMatrix, modelMatrix, glMatrix.toRadian(angle));
-            mat4.rotateZ(modelMatrix, modelMatrix, glMatrix.toRadian(angle));
+            mat4.rotateY(modelMatrix = mat4.create(), modelMatrix, glMatrix.toRadian(angle));
+            mat4.rotateX(modelMatrix, modelMatrix, glMatrix.toRadian(90));
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "uModel"), !1, modelMatrix);
 
             mat4.invert(temp = mat4.create(), modelMatrix);
             mat4.transpose(temp, temp);
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "uNormal"), !1, temp);
 
-            mat4.lookAt(temp = mat4.create(), [3, 3, 7], [0, 0, 0], [0, 1, 0]);
-            // mat4.lookAt(temp = mat4.create(), [0, 0, 7], [0, 0, 0], [0, 1, 0]);
+            mat4.lookAt(temp = mat4.create(), [3, 2, 8], [0, -1, 0], [0, 1, 0]);
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "uView"), !1, temp);
 
             mat4.perspective(temp = mat4.create(), glMatrix.toRadian(zoom == 30 ? zoom : zoom--), c.width / c.height, 1, 100);
             gl.uniformMatrix4fv(gl.getUniformLocation(program, "uProjection"), !1, temp);
 
+			gl.uniform3f(gl.getUniformLocation(program,"uLightDirection"), -1, -2.5, -5);
+			gl.uniform3f(gl.getUniformLocation(program,"uEyePosition"), 3 ,3 ,7);
             gl.uniform3f(gl.getUniformLocation(program, "uMaterialDiffuse"), (Math.sin(2 * Math.PI / 2550 * counter) * 127 + 128) / 255, (Math.sin(2 * Math.PI / 2550 * counter + 2) * 127 + 128) / 255, (Math.sin(2 * Math.PI / 2550 * counter + 4) * 127 + 128) / 255);
-            gl.uniform3f(gl.getUniformLocation(program, "uLightDiffuse"), 1, 1, 1);
-            gl.uniform3f(gl.getUniformLocation(program, "uLightPosition"), 5, 5, 7);
+		  	// gl.uniform3f(gl.getUniformLocation(program,"uMaterialDiffuse"), 0.2, 0.7, 0.2);
+			gl.uniform3f(gl.getUniformLocation(program,"uLightDiffuse"), 1, 1, 1);
+		  	gl.uniform3f(gl.getUniformLocation(program,"uMaterialAmbient"), 0.2, 0.2, 0.2);
+			gl.uniform3f(gl.getUniformLocation(program,"uLightAmbient"), 0.3, 0.3, 0.3);
+		  	gl.uniform3f(gl.getUniformLocation(program,"uMaterialSpecular"), 0.9, 1, 0.9);
+			gl.uniform3f(gl.getUniformLocation(program,"uLightSpecular"), 1, 1, 1);
+			gl.uniform1f(gl.getUniformLocation(program,"uShininess"), 1);
+			gl.uniform1i(gl.getUniformLocation(program,"uEnableAmbient"), !0);
+			gl.uniform1i(gl.getUniformLocation(program,"uEnableDiffuse"), !0);
+			gl.uniform1i(gl.getUniformLocation(program,"uEnableSpecular"), !0);
 
-            gl.clearColor(255, 255, 255, 1);
-            gl.enable(gl.DEPTH_TEST);
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
-            gl.drawElements(gl.TRIANGLES, vertices.length / 3, gl.UNSIGNED_SHORT, 0);
+		  	//draw scene
+			gl.clearColor(0, 0, 0, 1);
+			gl.enable(gl.DEPTH_TEST);
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+			gl.drawElements(gl.TRIANGLES, vertices.length / 3, gl.UNSIGNED_SHORT, 0);   
         },
         easing = function (t, b, c, d) {
             t /= d/2;
@@ -123,11 +131,11 @@
             angle = easing((counter++%200), angle % 360, 360, 3000);
             main();
         }, 10);
-    
+
     jarvis.learn([
         ["rotate", function () {
             interval = setInterval(function () {
-                angle = easing((counter++%200), angle % 360, 360, 3000);
+                angle = easing((counter%200), angle % 360, 360, 3000);
                 main();
             }, 10);
         }],
